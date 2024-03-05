f0ce4e45aa191e246d8f2a09fc75800c73777708
Luis David Guaiquirian <luisgcode@gmail.com>
2024-03-05 13:11:03 -0800

    Finally done

--- javascript/home.js
+++ javascript/home.js
@@ -1,76 +1,80 @@
 "use strict";
 //! Crowd meter
 document.addEventListener("DOMContentLoaded", () => {
-   //Clase que comparten los botones
-   const tabsDays = document.querySelectorAll(".crowd__tab-day");
-   //Clase del container padre de los botones
-   const tabsDaysContainer = document.querySelector(".schedule__inner-top");
-   //Clase que comparten los bloques de HTML a mostrar
-   const contentBoxMedium = document.querySelectorAll(".schedule__inner-medium");
+  //Clase que comparten los botones
+  const tabsDays = document.querySelectorAll(".crowd__tab-day");
+  //Clase del container padre de los botones
+  const tabsDaysContainer = document.querySelector(".schedule__inner-top");
+  //Clase que comparten los bloques de HTML a mostrar
+  const contentBoxMedium = document.querySelectorAll(".schedule__inner-medium");
 
-   // ? AGREGAMOS EVENTO AL PADRE DE LOS BOTONES //
+  // ? AGREGAMOS EVENTO AL PADRE DE LOS BOTONES //
 
-   tabsDaysContainer.addEventListener("click", function (e) {
-      const clicked = e.target.closest(".crowd__tab-day");
-      if (!clicked) return;
+  tabsDaysContainer.addEventListener("click", function (e) {
+    const clicked = e.target.closest(".crowd__tab-day");
+    if (!clicked) return;
 
-      tabsDays.forEach(function (t) {
-         t.classList.remove("crowd__tab-day--active");
-      });
+    tabsDays.forEach(function (t) {
+      t.classList.remove("crowd__tab-day--active");
+    });
 
-      contentBoxMedium.forEach(function (c) {
-         c.classList.remove("group__bar--active");
-      });
+    contentBoxMedium.forEach(function (c) {
+      c.classList.remove("group__bar--active");
+    });
 
-      clicked.classList.add("crowd__tab-day--active");
+    clicked.classList.add("crowd__tab-day--active");
 
-      document.querySelector(`.group__bar-${clicked.dataset.tab}`).classList.add("group__bar--active");
-   });
+    document
+      .querySelector(`.group__bar-${clicked.dataset.tab}`)
+      .classList.add("group__bar--active");
+  });
 });
 
 //! Carrousel of experience
 const sliderGrid = function () {
-   const slidesGrid = document.querySelectorAll(".feed__slider");
-   const btnLeftGrid = document.querySelector(".feed_btn--left");
-   const btnRightGrid = document.querySelector(".feed_btn--right");
+  const slidesGrid = document.querySelectorAll(".feed__slider");
+  const btnLeftGrid = document.querySelector(".feed_btn--left");
+  const btnRightGrid = document.querySelector(".feed_btn--right");
 
-   let curSlideGrid = 0;
-   // ? PARA QUE EL MAXIMO SEA EL NUMERO DE SLIDES USADOS //
-   const maxSlideGrid = slidesGrid.length;
+  let curSlideGrid = 0;
+  // ? PARA QUE EL MAXIMO SEA EL NUMERO DE SLIDES USADOS //
+  const maxSlideGrid = slidesGrid.length;
 
-   const goToSlide = function (slide) {
-      slidesGrid.forEach((s, i) => (s.style.transform = `translateX(${100 * (i - slide)}%)`));
-   };
+  const goToSlide = function (slide) {
+    slidesGrid.forEach(
+      (s, i) => (s.style.transform = `translateX(${100 * (i - slide)}%)`)
+    );
+  };
 
-   // Next slide
-   const nextSlide = function () {
-      if (curSlideGrid === maxSlideGrid - 1) {
-         curSlideGrid = 0;
-      } else {
-         curSlideGrid++;
-      }
+  // Next slide
+  const nextSlide = function () {
+    if (curSlideGrid === maxSlideGrid - 1) {
+      curSlideGrid = 0;
+    } else {
+      curSlideGrid++;
+    }
 
-      goToSlide(curSlideGrid);
-   };
+    goToSlide(curSlideGrid);
+  };
 
-   const prevSlide = function () {
-      if (curSlideGrid === 0) {
-         curSlideGrid = maxSlideGrid - 1;
-      } else {
-         curSlideGrid--;
-      }
-      goToSlide(curSlideGrid);
-   };
+  const prevSlide = function () {
+    if (curSlideGrid === 0) {
+      curSlideGrid = maxSlideGrid - 1;
+    } else {
+      curSlideGrid--;
+    }
+    goToSlide(curSlideGrid);
+  };
 
-   const init = function () {
-      goToSlide(0);
-   };
+  const init = function () {
+    goToSlide(0);
+  };
 
-   init();
+  init();
 
-   // Event handlers
-   btnRightGrid.addEventListener("click", nextSlide);
-   btnLeftGrid.addEventListener("click", prevSlide);
+  // Event handlers
+  btnRightGrid.addEventListener("click", nextSlide);
+  btnLeftGrid.addEventListener("click", prevSlide);
 };
 
 sliderGrid();
